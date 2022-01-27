43dab364e04772d4838eeb4398a5f76dd53d194b
Deepan <deepan06102000@gmail.com>
2022-01-27 21:23:57 +0530

    input filter combo boxes in table working

--- src/InvertedMatchingDemo.java
+++ src/InvertedMatchingDemo.java
@@ -29,19 +29,19 @@ public class InvertedMatchingDemo {
     	
     	Hashtable<String, String> doi_dict = PDFParserClass.PDFtoTXTMain(directoryName, TXTfilesLoc);
     	System.out.print("###########DONE###############");
-    	System.out.println("The set is: " + doi_dict.toString());
-    	
-    	doi_dict.keys().asIterator()
-        .forEachRemaining(s -> System.out.println(doi_dict.get(s)));
-		
-//        InvertedIndexingMain.GenerateInvertedIndex(TXTfilesLoc,InvertedIndexFile);
-//
-//    	ConcurrentHashMap<String, String[]> IndexLines=InvertedIndexParser.ReadIndexLines(InvertedIndexFile);
-//    	System.out.print(IndexLines.size());
+//    	System.out.println("The set is: " + doi_dict.toString());
 //    	
-//    	ConcurrentHashMap<String, String> MatchWords =MatchingIndex.GetMatchingWordsFromII(InvertedIndexFile, InvertedSearchWord);
-//    	System.out.println("Count of Matched Words: "+Integer.toString(MatchWords.size()));
-//    	System.out.println("The set is: " + MatchWords.toString());
+//    	doi_dict.keys().asIterator()
+//        .forEachRemaining(s -> System.out.println(doi_dict.get(s)));
+		
+        InvertedIndexingMain.GenerateInvertedIndex(TXTfilesLoc,InvertedIndexFile);
+
+    	ConcurrentHashMap<String, String[]> IndexLines=InvertedIndexParser.ReadIndexLines(InvertedIndexFile);
+    	System.out.print(IndexLines.size());
+    	
+    	ConcurrentHashMap<String, String> MatchWords =MatchingIndex.GetMatchingWordsFromII(InvertedIndexFile, InvertedSearchWord);
+    	System.out.println("Count of Matched Words: "+Integer.toString(MatchWords.size()));
+    	System.out.println("The set is: " + MatchWords.toString());
     	
 //    	FOR JUST WORDS LIST
 //    	BestMatchingData result;
--- src/com/journalbuddy/SwingUI/JournalBuddyFront.java
+++ src/com/journalbuddy/SwingUI/JournalBuddyFront.java
@@ -6,8 +6,15 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableColumn;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 
+import com.journalbuddy.MatchingVocab.MatchingIndex;
 import com.journalbuddy.PDFParser.PDFParserClass;
+import com.journalbuddy.invertedindexing.InvertedIndexParser;
+import com.journalbuddy.invertedindexing.InvertedIndexingMain;
 
 import java.awt.Color;
 import javax.swing.JLabel;
@@ -18,7 +25,9 @@ import javax.swing.JTable;
 //import com.jgoodies.forms.layout.ColumnSpec;
 //import com.jgoodies.forms.layout.RowSpec;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 
 import java.awt.event.ActionListener;
@@ -28,8 +37,11 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.awt.event.ActionEvent;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTree;
 
 public class JournalBuddyFront extends JFrame {
 
@@ -37,6 +49,11 @@ public class JournalBuddyFront extends JFrame {
 	private JTable dirtable;
 	private JTable doitable;
 	private String TXTfilesLoc="E:\\Research Papers\\New folder";
+	private String InvertedIndexFile="E:\\BOOKS DUMP\\JAVA\\Parallel\\MainProjects\\op.txt";
+	private JTextField wordfield;
+	private JTable filterstable;
+	
+	List<TableCellEditor> editors = new ArrayList<TableCellEditor>(3);
 	/**
 	 * Launch the application.
 	 */
@@ -171,9 +188,129 @@ public class JournalBuddyFront extends JFrame {
 		
 		JPanel wordsearch = new JPanel();
 		maintabpane.addTab("Deep Search for Journals", null, wordsearch, null);
+		wordsearch.setLayout(null);
 		
-		JPanel filtering = new JPanel();
-		maintabpane.addTab("Filter Journal Data", null, filtering, null);
+		JLabel lblNewLabel = new JLabel("Enter a Word to search: ");
+		lblNewLabel.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 20));
+		lblNewLabel.setBounds(113, 44, 207, 39);
+		wordsearch.add(lblNewLabel);
+		
+		wordfield = new JTextField();
+		wordfield.setBounds(330, 51, 730, 34);
+		wordsearch.add(wordfield);
+		wordfield.setColumns(10);
+		
+		JTree tree = new JTree();
+		tree.setBounds(113, 150, 1144, 503);
+		wordsearch.add(tree);
+    	DefaultTreeModel treemodel = (DefaultTreeModel)tree.getModel();
+    	DefaultMutableTreeNode root = (DefaultMutableTreeNode)treemodel.getRoot();
+	    root.removeAllChildren();
+	    treemodel.reload();
+	    
+		JButton wordsearchbutton = new JButton("Search");
+		wordsearchbutton.setBounds(1093, 44, 164, 39);
+		wordsearchbutton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+		    	DefaultTreeModel model = (DefaultTreeModel)tree.getModel();
+		    	DefaultMutableTreeNode root = (DefaultMutableTreeNode)model.getRoot();
+			    root.removeAllChildren();
+			    model.reload();
+		        InvertedIndexingMain.GenerateInvertedIndex(TXTfilesLoc,InvertedIndexFile);
+
+		    	ConcurrentHashMap<String, String[]> IndexLines=InvertedIndexParser.ReadIndexLines(InvertedIndexFile);
+		    	System.out.print(IndexLines.size());
+		    	
+		    	ConcurrentHashMap<String, String> MatchWords =MatchingIndex.GetMatchingWordsFromII(InvertedIndexFile, wordfield.getText());
+		    	System.out.println("Count of Matched Words: "+Integer.toString(MatchWords.size()));
+		    	System.out.println("The set is: " + MatchWords.toString());
+		    	
+		    	DefaultMutableTreeNode mainword=new DefaultMutableTreeNode("Search Word: "+wordfield.getText());
+		    	Set<String> setOfKeys = MatchWords.keySet();
+		    	for (String key : setOfKeys) {
+		    		DefaultMutableTreeNode foundword=new DefaultMutableTreeNode("Found Similar Word: "+key);
+		    		mainword.add(foundword);
+		    		String[] foundfiles=MatchWords.get(key).split(";");
+		    		for(int k = 0;k<foundfiles.length;k++) {
+		    			DefaultMutableTreeNode foundfile=new DefaultMutableTreeNode("Found in File: "+foundfiles[k]);
+		    			foundword.add(foundfile);
+		    		}
+		    		
+		    	}
+		    	
+		    	root.add(mainword);
+		    	model.reload(root);
+		    	
+			}
+			
+		});
+		wordsearch.add(wordsearchbutton);
+		
+		JTabbedPane datafilterr = new JTabbedPane(JTabbedPane.TOP);
+		maintabpane.addTab("Data Filter", null, datafilterr, null);
+		
+		JPanel inputfilters = new JPanel();
+		datafilterr.addTab("Input Filters", null, inputfilters, null);
+		inputfilters.setLayout(null);
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		scrollPane_2.setBounds(10, 11, 1332, 594);
+		inputfilters.add(scrollPane_2);
+		
+		
+        String[] attrs = { "", "File_Name", "DOI", "Title", "Container_Name", "Author_FirstName",
+        		"Author_LastName", "Single_Subject", "Single_Funder", "Single_Award", "Author_Sequence",
+        		"Volume", "Issue", "Is_Referenced_By_Count", "Reference_count", "Published_Date", "Issued_Date"};
+        JComboBox<String> comboBox1 = new JComboBox<String>( attrs );
+        DefaultCellEditor dce1 = new DefaultCellEditor( comboBox1 );
+        editors.add( dce1 );
+
+        String[] items2 = { "", "equals", "greater_than", "lesser_than" };
+        JComboBox<String> comboBox2 = new JComboBox<String>( items2 );
+        DefaultCellEditor dce2 = new DefaultCellEditor( comboBox2 );
+        editors.add( dce2 );
+        
+		DefaultTableModel filterstablemodel = new DefaultTableModel();
+		filterstablemodel.addColumn("Attribute");
+		filterstablemodel.addColumn("Value");
+		filterstablemodel.addColumn("Filter Parameter");
+		filterstablemodel.addColumn("Max. Word Difference");
+		
+		filterstable = new JTable(filterstablemodel) {
+            public TableCellEditor getCellEditor(int row, int column)
+            {
+                int modelColumn = convertColumnIndexToModel( column );
+
+                if (column==0)
+                    return editors.get(0);
+                else if (column==2)
+                    return editors.get(1);
+                else
+                    return super.getCellEditor(row, column);
+            }
+        };
+		scrollPane_2.setViewportView(filterstable);
+		
+		JButton filteraddrow = new JButton("Add a Row");
+		filteraddrow.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 15));
+		filteraddrow.setBounds(264, 616, 199, 60);
+		filteraddrow.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				filterstablemodel.addRow(new Object[] {"", "", "", ""});
+			}
+			
+		});
+		inputfilters.add(filteraddrow);
+		
+		JButton btnApplyFilters = new JButton("Apply Filters");
+		btnApplyFilters.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 15));
+		btnApplyFilters.setBounds(892, 616, 199, 60);
+		inputfilters.add(btnApplyFilters);
+		add_dir.setLayout(null);
+		
+		JPanel filteroutput = new JPanel();
+		datafilterr.addTab("View Filter Output", null, filteroutput, null);
+		add_dir.setLayout(null);
 		
 	}
 }
